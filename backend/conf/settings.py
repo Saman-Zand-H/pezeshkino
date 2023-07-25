1b61c2ad1eb6f38031f5380047a3fdc67df102e2
DJS <tnsperuse@gmail.com>
2023-07-25 18:50:56 +0330

    preparing for deployment

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -32,7 +32,7 @@ sys.path.append(os.path.join(BASE_DIR, 'apps'))
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
